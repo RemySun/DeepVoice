e83c509f0195832193e4de290cf01c270d21d0ec
Rémy <Remy.Sun@ens-rennes.fr>
2017-01-24 18:50:58 +0100

    threshold method

--- firstNet/Autoencoder.py
+++ firstNet/Autoencoder.py
@@ -5,13 +5,16 @@ import random
 import pandas as pd
 import sys
 import pickle
+import sys
+
+sys.stdout = open('training.log', 'w')
 
 print("Loading data...")
 
 #data = pd.io.parsers.read_csv("data.csv")
 #data = data.as_matrix()
-data = pickle.load(open("data.p", "rb"))
-labels = pickle.load(open("labels.p", "rb"))
+data = pickle.load(open("train_data.p", "rb"))
+labels = pickle.load(open("train_labels.p", "rb"))
 
 print("Preparing data...")
 
@@ -36,9 +39,8 @@ print("Generating neural network...")
 n_samp = len(input)
 n_input = len(input[0])
 
-n_layer1 = 5000
-n_layer2 = 1000
-n_layer3 = 400
+n_layer1 = 2000
+n_layer2 = 500
 n_hidden = 50
 
 x = tf.placeholder("float", [None, n_input])
@@ -56,48 +58,34 @@ bl2 = tf.Variable(tf.zeros([n_layer2]))
 l2 = tf.nn.tanh(tf.matmul(l1_drop,Wl2) + bl2)
 l2_drop = tf.nn.dropout(l2, keep_prob=0.90)
 
-# Weights and biases to third layer
-
-Wl3 = tf.Variable(tf.random_uniform((n_layer2, n_layer3), -1.0 / math.sqrt(n_layer2), 1.0 / math.sqrt(n_layer2)))
-bl3 = tf.Variable(tf.zeros([n_layer3]))
-l3 = tf.nn.tanh(tf.matmul(l2_drop,Wl3) + bl3)
-l3_drop = tf.nn.dropout(l3, keep_prob=0.90)
-
 # Weights and biases to hidden layer
 
-Wh = tf.Variable(tf.random_uniform((n_layer3, n_hidden), -1.0 / math.sqrt(n_layer3), 1.0 / math.sqrt(n_layer3)))
+Wh = tf.Variable(tf.random_uniform((n_layer2, n_hidden), -1.0 / math.sqrt(n_layer2), 1.0 / math.sqrt(n_layer2)))
 bh = tf.Variable(tf.zeros([n_hidden]))
-h = tf.nn.tanh(tf.matmul(l3_drop,Wh) + bh)
+h = tf.nn.tanh(tf.matmul(l2_drop,Wh) + bh)
 h_drop = tf.nn.dropout(h, keep_prob=0.90)
 
 # Weights and biases to fifth layer tied to hidden
 
-Wl5 = tf.transpose(Wh)
-bl5 = tf.Variable(tf.zeros([n_layer3]))
-l5 = tf.nn.tanh(tf.matmul(h_drop,Wl5) + bl5)
-l5_drop = tf.nn.dropout(l5, keep_prob=0.90)
-
-# Weights and biases to sixth layer tied to third
-
-Wl6 = tf.transpose(Wl3)
-bl6 = tf.Variable(tf.zeros([n_layer2]))
-l6 = tf.nn.tanh(tf.matmul(l5_drop,Wl6) + bl6)
-l6_drop = tf.nn.dropout(l6, keep_prob=0.90)
+Wl4 = tf.transpose(Wh)
+bl4 = tf.Variable(tf.zeros([n_layer2]))
+l4 = tf.nn.tanh(tf.matmul(h_drop,Wl4) + bl4)
+l4_drop = tf.nn.dropout(l4, keep_prob=0.90)
 
 # Weights and biases to seventh layer tied to first
 
-Wl7 = tf.transpose(Wl2)
-bl7 = tf.Variable(tf.zeros([n_layer1]))
-l7 = tf.nn.tanh(tf.matmul(l6_drop,Wl7) + bl7)
-l7_drop = tf.nn.dropout(l7, keep_prob=0.90)
+Wl5 = tf.transpose(Wl2)
+bl5 = tf.Variable(tf.zeros([n_layer1]))
+l5 = tf.nn.tanh(tf.matmul(l4_drop,Wl5) + bl5)
+l5_drop = tf.nn.dropout(l5, keep_prob=0.90)
 
 # Weights and biases to output layer
 Wo = tf.Variable(tf.random_uniform((n_layer1, n_input), -1.0 / math.sqrt(n_layer1), 1.0 / math.sqrt(n_layer1)))
 bo = tf.Variable(tf.zeros([n_input]))
-y = tf.nn.tanh(tf.matmul(l7_drop,Wo) + bo)
+y = tf.nn.tanh(tf.matmul(l5_drop,Wo) + bo)
 
 # Objective functions
-y_ = tf.placeholder("float", [None,1])
+y_ = tf.placeholder("float", [None,n_input])
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
 meansq = tf.reduce_mean(tf.square(y_-y))
 train_step = tf.train.GradientDescentOptimizer(0.05).minimize(meansq)
@@ -110,27 +98,26 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
-ckpt = tf.train.get_checkpoint_state("model.ckpt")
-if ckpt and ckpt.model_checkpoint_path:
-    print("Loading trained model...")
-    saver.restore(sess, ckpt.model_checkpoint_path)
-else:
-    print("No trained model to load... Starting from scratch!")
+#print("Loading trained model...")
+#saver.restore(sess, "model.ckpt")
 
 n_rounds = 30
 batch_size = min(50, n_samp)
 
 print("Training...")
 
+batch_xs = np.zeros((50,n_input))
+batch_ys = np.zeros((50,n_input))
+
 for i in range(n_rounds):
+    print("Starting epoch ", i)
     np.random.shuffle(index)
     for j in range(n_samp // batch_size -1):
-        print("Starting epoch ", i)
-        batch_xs = np.array([input[ind][:] for ind in index[j*batch_size:j*(batch_size+1)-1]])
-        batch_ys = np.array([output[ind][:] for ind in index[j*batch_size:j*(batch_size+1)-1]])
-        print("training on batch of size ", len(batch_xs))
+        for k in range(batch_size):
+            batch_xs[k][:] = input[index[j*batch_size + k]]
+            batch_ys[k][:] = output[index[j*batch_size + k]]
         sess.run(train_step, feed_dict={x: batch_xs, y_:batch_ys})
-        print("round ", i, " batch ", j," out of ",n_samp//batch_size, " with error ", sess.run(meansq, feed_dict={x: batch_xs, y_:batch_ys}))
+        print("Epoch ", i, " batch ", j," out of ",n_samp//batch_size, " with error ", sess.run(meansq, feed_dict={x: batch_xs, y_:batch_ys}))
     # Save the variables to disk.
     save_path = saver.save(sess, "model.ckpt")
     print("Model saved in file: %s" % save_path)
--- firstNet/sillyNorm.py
+++ firstNet/sillyNorm.py
@@ -8,44 +8,116 @@ import pickle
 
 print("Loading data...")
 
-data = pd.io.parsers.read_csv("data.csv")
-data = data.as_matrix()
+data = pickle.load(open("valid_data.p", "rb"))
+labels = pickle.load(open("valid_labels.p", "rb"))
 
-labels = pickle.load(open("labels.p", "rb"))
+print("Generating neural network...")
 
-print("Preparing data...")
+# Autoencoder with 3 hidden layer
+n_input = len(data[0])
+
+n_layer1 = 2000
+n_layer2 = 500
+n_hidden = 50
+
+x = tf.placeholder("float", [None, n_input])
+
+# Weights and biases to first layer
+Wl1 = tf.Variable(tf.random_uniform((n_input, n_layer1), -1.0 / math.sqrt(n_input), 1.0 / math.sqrt(n_input)))
+bl1 = tf.Variable(tf.zeros([n_layer1]))
+l1 = tf.nn.tanh(tf.matmul(x,Wl1) + bl1)
+l1_drop = tf.nn.dropout(l1, keep_prob=0.90)
+
+# Weights and biases to second layer
+
+Wl2 = tf.Variable(tf.random_uniform((n_layer1, n_layer2), -1.0 / math.sqrt(n_layer1), 1.0 / math.sqrt(n_layer1)))
+bl2 = tf.Variable(tf.zeros([n_layer2]))
+l2 = tf.nn.tanh(tf.matmul(l1_drop,Wl2) + bl2)
+l2_drop = tf.nn.dropout(l2, keep_prob=0.90)
+
+# Weights and biases to hidden layer
+
+Wh = tf.Variable(tf.random_uniform((n_layer2, n_hidden), -1.0 / math.sqrt(n_layer2), 1.0 / math.sqrt(n_layer2)))
+bh = tf.Variable(tf.zeros([n_hidden]))
+h = tf.nn.tanh(tf.matmul(l2_drop,Wh) + bh)
+h_drop = tf.nn.dropout(h, keep_prob=0.90)
+
+# Weights and biases to fifth layer tied to hidden
+
+Wl4 = tf.transpose(Wh)
+bl4 = tf.Variable(tf.zeros([n_layer2]))
+l4 = tf.nn.tanh(tf.matmul(h_drop,Wl4) + bl4)
+l4_drop = tf.nn.dropout(l4, keep_prob=0.90)
+
+# Weights and biases to seventh layer tied to first
+
+Wl5 = tf.transpose(Wl2)
+bl5 = tf.Variable(tf.zeros([n_layer1]))
+l5 = tf.nn.tanh(tf.matmul(l4_drop,Wl5) + bl5)
+l5_drop = tf.nn.dropout(l5, keep_prob=0.90)
+
+# Weights and biases to output layer
+Wo = tf.Variable(tf.random_uniform((n_layer1, n_input), -1.0 / math.sqrt(n_layer1), 1.0 / math.sqrt(n_layer1)))
+bo = tf.Variable(tf.zeros([n_input]))
+y = tf.nn.tanh(tf.matmul(l5_drop,Wo) + bo)
+
+# Objective functions
+y_ = tf.placeholder("float", [None,n_input])
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+meansq = tf.reduce_mean(tf.square(y_-y))
+train_step = tf.train.GradientDescentOptimizer(0.05).minimize(meansq)
+
+# Add ops to save and restore all the variables.
+saver = tf.train.Saver()
+
+
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+print("Loading trained model...")
+saver.restore(sess, "model.ckpt")
+
+print("Computing intermediate representations")
+
+input = np.array(data)
+
+deep_vectors = sess.run(h, feed_dict={x: input})
 
 pairs = []
 
-for i in range(data.shape[0]-1):
-    for j in range(data.shape[0]-i-1):
+for i in range(len(deep_vectors)-1):
+    for j in range(len(deep_vectors)-i-1):
         if labels[i] == labels[i+j+1]:
-            pairs.append([data[i],data[i+j+1]])
+            pairs.append([deep_vectors[i],deep_vectors[i+j+1]])
 
-# Code here for importing data from file
 
-input = [p[0] for p in pairs] + [p[1] for p in pairs]
-
-output = [p[1] for p in pairs] + [p[0] for p in pairs]
+print("Computing threshold...")
 
 def norm(a):
     d = 0
     for i in a:
         d += i**2
-    d
+    return np.sqrt(d)
 
-dist_same = [dist(p[0] - p[1]) for p in pairs]
+def cosine(a,b):
+    dot = [i*j for i,j in zip(a,b)]
+    return sum([d/(norm(a)*norm(b)) for d in dot])
 
-threshold = max(dist_same)
+dist_same = [cosine(p[0],p[1]) for p in pairs]
+
+threshold = min(dist_same)
 
 oops = 0
 alright = 0
 
-for i in range(data.shape[0]-1):
-    for j in range(data.shape[0]-i-1):
-        if labels[i] != labels[i+j+1]:
-            tmp = dist(data[i]-data(i+j+1))
-            if tmp < threshold:
+print("Computing results...")
+
+for i in range(len(deep_vectors)):
+    for j in range(len(deep_vectors)-i):
+        if labels[i] != labels[i+j]:
+            tmp = cosine(deep_vectors[i],deep_vectors[i+j])
+            if tmp >= threshold:
                 oops += 1
             else:
                 alright += 1
