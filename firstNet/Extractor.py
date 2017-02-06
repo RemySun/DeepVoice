5357bb3efd11971c9f0e1ba900cf183ffc280d96
Rémy <Remy.Sun@ens-rennes.fr>
2017-02-06 02:34:42 +0100

    updated extractor'

--- firstNet/Extractor.py
+++ firstNet/Extractor.py
@@ -40,18 +40,34 @@ for i, s in enumerate(Files):
     ## Store supervector
     data.append(supervector)
 
+train_data = []
+train_labels = []
+valid_data = []
+valid_labels = []
+
+for i in range(len(data)):
+    if i % 20 == 0:
+        valid_data.append(data[i])
+        valid_labels.append(labels[i])
+    else:
+        train_data.append(data[i])
+        train_labels.append(labels[i])
+
 print("Dumping data")
 
 # Write data and labels
-f_data = open("data.p", "wb")
-f_labels = open("labels.p", "wb")
+t_data = open("train_data.p", "wb")
+t_labels = open("train_labels.p", "wb")
+v_data = open("valid_data.p", "wb")
+v_labels = open("valid_labels.p", "wb")
 
-#w_data = csv.writer(f_data)
+pickle.dump(train_data, t_data)
+pickle.dump(train_labels, t_labels)
 
-pickle.dump(data, f_data)
-pickle.dump(labels, f_labels)
+pickle.dump(valid_data, v_data)
+pickle.dump(valid_labels, v_labels)
 
-#w_data.writerows(data)
-
-f_data.close()
-f_labels.close()
+t_data.close()
+t_labels.close()
+v_data.close()
+v_labels.close()
